b39d6def3a3be76397d40154e4230d2e3476719b
Claudio Melgaco <claudio19a@hotmail.com>
2023-02-09 13:42:48 -0300

    erro do lint corrigido

--- src/interfaces/products.ts
+++ src/interfaces/products.ts
@@ -1,4 +1,6 @@
 export interface IProduct {
+  id: number,
   name: string;
   amount: string;
+  orderId: number,
 }
--- src/models/productsModels.ts
+++ src/models/productsModels.ts
@@ -1,10 +1,17 @@
 import { RowDataPacket } from 'mysql2';
+import { Pool } from 'mysql2/promise';
 import connection from './connection';
 import { IProduct } from '../interfaces/products';
 
 export default class ProductModel {
-  async getAll(): Promise<IProduct[]> {
-    const [result] = await connection.execute<(IProduct & RowDataPacket)[]>(
+  public connection: Pool;
+
+  constructor() {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<IProduct[]> {
+    const [result] = await this.connection.execute<(IProduct & RowDataPacket)[]>(
       'SELECT * FROM Trybesmith.products ');
 
     return result;
