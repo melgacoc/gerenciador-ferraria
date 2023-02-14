4b53b696e90812b18d284e6af55c27663437c0f5
Cláudio <claudio19a@hotmail.com>
2023-02-14 12:39:01 -0300

    adequando lint do req 03

--- src/models/usersModels.ts
+++ src/models/usersModels.ts
@@ -1,12 +1,17 @@
-import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
 import { IUser } from '../interfaces/users';
 
 export default class UserModel {
-  
-  async newUser(newUser: IUser): Promise<IUser> {
+  public connection;
+
+  constructor() {
+    this.connection = connection;
+  }
+
+  public async newUser(newUser: IUser): Promise<IUser> {
     const { username, vocation, level, password } = newUser;
-    const [result] = await connection.execute<ResultSetHeader>(
+    const [result] = await this.connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.users (username, vocation, level, password) VALUES (?, ?, ?, ?)',
       [username, vocation, level, password],
     );
